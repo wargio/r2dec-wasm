0b2d79812905a6d0d8f333d70c1d3df16b9a8e9f
Giovanni <561184+wargio@users.noreply.github.com>
2018-12-03 10:18:21 +0100

    Update travis-test.sh

--- travis-test.sh
+++ travis-test.sh
@@ -6,18 +6,6 @@ if [[  "$ERRORED" == "1" ]]; then
 	exit $ERRORED
 fi
 
-## r2dec-regression
-cd ..
-WORKINGDIR_TRAVIS=$(pwd)
-ls r2dec-js >/dev/null 2>&1 || git clone --depth 1 https://github.com/wargio/r2dec-js
-rm -rf r2dec-regression >/dev/null 2>&1 || echo "no need to clean.."
-git clone --branch "$TRAVIS_BRANCH" --depth 1 https://github.com/wargio/r2dec-regression || git clone --depth 1 https://github.com/wargio/r2dec-regression
-cd r2dec-regression
-chmod +x testall.sh
-./testall.sh "$WORKINGDIR_TRAVIS/r2dec-js" travis
-ERRORED=$?
-cd ..
-
 ## NPM eslint
 npm install -s eslint
 
